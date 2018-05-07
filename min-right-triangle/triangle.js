2d86bcc0a13ec6f3be2b3c28f6df14f7065c85fc
Ostanin Igor <ostanin.igor@skbkontur.ru>
2018-05-07 20:56:54 +0500

    helper functions

--- min-right-triangle/triangle.js
+++ min-right-triangle/triangle.js
@@ -10,18 +10,60 @@ const polygone = [
     {x: 200, y: 100},
 ].map(({x, y}) => new Point(x + 100, y + 100))
 
+
+Array.prototype.find = function find(key=(x => x), keycmp=((x, y) => x == y)) {
+    let elem = this[0],
+        keyv = key(elem)
+    this.slice(1).forEach(e => {
+        const keye = key(e)
+        if (keycmp(keye, keyv)) {
+            elem = e
+            keyv = keye
+        }
+    })
+    return elem
+}
+
+
+Array.prototype.min = function min(key=(x => x)) {
+    return this.find(key, (x, y) => x < y)
+}
+
+Array.prototype.max = function max(key=(x => x)) {
+    return this.find(key, (x, y) => x >= y)
+}
+
+Array.prototype.ngrams = function(n) {
+    if (n == 1) {
+        return this.map(e => e)
+    }
+    return this.slice(0, 1 - n).map((_, i) => this.slice(i, i + n))
+}
+
 /**
  * Returns a vertices of a minimal right triangle that covers given polygone.
  *
  * @param polygone [Point]target to be covered with right triangle. 
- * @returns [Point] of size 3 which contains coordinates of minimal triangle.
+ * @returns [Point] of size 3 which contains vertices of a minimal triangle.
  */
 function find_minimal_right_triangle(polygone) {
+    switch (polygone.length) {
+        case 0: return []
+        case 1: return polygone.concat(polygone).concat(polygone)
+        case 2: const [a, b] = polygone
+        return [ a, b, a.add(b).stratch(1/2)
+            .add(a.sub(b).stratch(1/2).rot(Math.PI/2)) ]
+        case 3: return polygone.map(e => e)
+    }
+
+    const edges = polygone.concat(polygone[0]).ngrams(2).map(([a, b]) => b.sub(a))
+
     return [
-        {x: 100, y: 100},
-        {x: 300, y: 100},
-        {x: 100, y: 300}
-    ].map(({x, y}) => new Point(x + 100, y + 100))
+        polygone.min(p => p.x),
+        polygone.max(p => p.y),
+        polygone.max(p => p.x),
+        polygone.min(p => p.y),
+    ]
 
 }
 
@@ -60,7 +102,6 @@ function drawPolygone(ctx, polygone) {
 
 function redraw() {
     const ctx = canvas.getContext('2d')
-    // ctx.globalCompositeOperation = 'destination-over'
 
     const {width, height} = canvas
     ctx.clearRect(0, 0, width, height)
@@ -70,7 +111,7 @@ function redraw() {
     drawPolygone(ctx, polygone)
 
     ctx.strokeStyle = 'black'
-    ctx.fillStyle = 'rgba(255, 255, 0, 0.3)'
+    ctx.fillStyle = 'rgba(255, 255, 0, 0.7)'
     drawPolygone(ctx, find_minimal_right_triangle(polygone))
 }
 
